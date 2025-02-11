6f643b5e3bef80fa6166ed623a9762afa2268cbb
MtKana <kana95372@gmail.com>
2025-02-11 19:33:53 +0900

    fix utility function

--- utilityFunctions.py
+++ utilityFunctions.py
@@ -63,12 +63,12 @@ def sort_files_in_directory(directory_path):
 #   color_labels: dictionary, dictionary of colours and their ids (x/y axis position), {colour:id}
 # OUTPUTS:
 #   Returns nothing, just plots
-def show_heatmaps2(vmin_val, vmax_val, matrices, rows, cols, titles, cbar_label=None, color_labels=None):
-    num_plots = len(matrices)
-    grid_size = math.ceil(math.sqrt(num_plots))  # Determine the grid size
-    fig, axs = plt.subplots(rows, cols, figsize=(16, 9)) #figsize=(5 * grid_size, 5 * grid_size))
+# def show_heatmaps2(vmin_val, vmax_val, matrices, rows, cols, titles, cbar_label=None, color_labels=None):
+#     num_plots = len(matrices)
+#     grid_size = math.ceil(math.sqrt(num_plots))  # Determine the grid size
+#     fig, axs = plt.subplots(rows, cols, figsize=(16, 9)) #figsize=(5 * grid_size, 5 * grid_size))
 
-#     # Flatten the axes array if it is 2D
+#    # Flatten the axes array if it is 2D
 #     if isinstance(axs, np.ndarray):
 #         axs = axs.ravel()
 #     else:
@@ -77,18 +77,18 @@ def show_heatmaps2(vmin_val, vmax_val, matrices, rows, cols, titles, cbar_label=
 #     for i, (matrix, title) in enumerate(zip(matrices, titles)):
 #         ax = axs[i]
         
-        im = ax.imshow(matrix, aspect='auto', vmin=vmin_val, vmax=vmax_val)
-        ax.set_title(title, fontsize=14)
+#        im = ax.imshow(matrix, aspect='auto', vmin=vmin_val, vmax=vmax_val)
+#        ax.set_title(title, fontsize=14)
 
 #         # Set axis labels
 #         ax.set_xlabel("Right")  # Label for x-axis
 #         ax.set_ylabel("Left")   # Label for y-axis
 
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes("right", size="5%", pad=0.05)
-        cbar = fig.colorbar(im, cax=cax)
-        cbar.set_label(cbar_label, fontsize=14)
-        cbar.ax.tick_params(labelsize=14)
+        # divider = make_axes_locatable(ax)
+        # cax = divider.append_axes("right", size="5%", pad=0.05)
+        # cbar = fig.colorbar(im, cax=cax)
+        # cbar.set_label(cbar_label, fontsize=14)
+        # cbar.ax.tick_params(labelsize=14)
 
 #         # Adjust the height of the color bar
 #         position = cax.get_position()

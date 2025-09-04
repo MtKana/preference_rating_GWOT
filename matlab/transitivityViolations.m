6e84f163b2e2ae104853b5711e50c5a94c49e266
Prototype003 <prototype032@gmail.com>
2025-09-04 11:36:28 +1000

    fixed incorrect use of binarised matrix - now uses masked/thresholded ratings matrix

--- matlab/transitivityViolations.m
+++ matlab/transitivityViolations.m
@@ -1,9 +1,8 @@
-function [violation_mat, b_thresholds, valid_mat] = transitivityViolations(rating_mats)
+function [violation_mat, b_thresholds, bmats, relevant_mat] = transitivityViolations(rating_mats)
 %transitivityViolations
 % transitivity: if x <= y  and y <= z, then x <= z
 % Supposed to be used with ratings that are antisymmetric
 %	(i.e. (a,b) and (b,a) have equal magnitude)
-%	(would still "work" if not antisymmetric though)
 %
 % Inputs:
 %	rating_mats = stim x stim x participants matrix
@@ -11,12 +10,21 @@ function [violation_mat, b_thresholds, valid_mat] = transitivityViolations(ratin
 %	violation_mat = stim x stim x stim x participants x thresholds matrix
 %		Holds 1 where transitivity was violated
 %	b_thresholds = vector of thresholds used for determining preference
-%	valid_mat = stim x stim x stim x participants x thresholds matrix
+%	bmats = stim x stim x participants x thresholds matrix
+%		Logical mask - zeros ratings which are within the thresholded range
+%	relevant_mat = stim x stim x stim x participants x thresholds matrix
 %		Holds 1 where the first two transitivity conditions are satisfied
+%		Note that because/if ratings are asymmetric, (a,b) = -(b,a)
+%			If (a,b) is negative, a is preferred (and so (b,a) should be
+%				positive)
+%		So unless (a,b)==(b,a)==0, only one of them will be relevant
 
 % Check first two conditions, then the third condition
 
-% set of possible thresholds
+% Signs of the ratings (ignoring magnitudes)
+rating_signs = sign(rating_mats);
+
+% Set of possible thresholds
 b_thresholds = unique(abs(rating_mats(:)));
 
 % Set of colour indexes
@@ -24,28 +32,43 @@ colours = (1:size(rating_mats, 1));
 
 % x * y * z * participants * thresholds
 violation_mat = zeros(numel(colours), numel(colours), numel(colours), size(rating_mats, 3), numel(b_thresholds));
-valid_mat = zeros(size(violation_mat)); % for counting where the first two conditions are true
+relevant_mat = zeros(size(violation_mat)); % for counting where the first two conditions are true
 
 bmats = nan([size(rating_mats, 1) size(rating_mats, 2) size(rating_mats, 3) numel(b_thresholds)]);
 for b = 1 : numel(b_thresholds)
 	b_thresh = b_thresholds(b);
 	
-	% Binarise ratings matrices
+	% "Clear preference" mask
 	% If rating is within range (-thresh, thresh), then consider it as no
 	% preference
 	bmats(:, :, :, b) = (rating_mats <= -b_thresh) | (rating_mats >= b_thresh);
 	
+	% zero-out ratings which were within the threshold range
+	rating_thresholded = bmats(:, :, :, b) .* rating_signs;
+	
 	for p = 1 : size(rating_mats, 3)
+		
+		% For each colour pair, select a third colour
+		% Note that because/if ratings are asymmetric, (a,b) = -(b,a)
+		%	So at most only one of (a,b) and (b,a) will be relevant
 		for x = colours
 			for y = colours(colours~=x)
+				
 				for z = colours(colours~=x & colours~=y)
 					
-					if bmats(x, y, p, b) == 1 && bmats(y, z, p, b) == 1
+					% If (x,y) is negative, x is preferred
+					%	and x <= y
+					% If (x,y) is positive, y is preferred (y<x)
+					%	and x !<= y
+					% If (x,y) is 0, neither were preferred (x=y)
+					%	and x <= y
+					
+					if rating_thresholded(x, y, p) <= 0 && rating_thresholded(y, z, p) <= 0
 						% Then the first two conditions are satisfied
 						
-						valid_mat(x, y, z, p, b) = 1;
+						relevant_mat(x, y, z, p, b) = 1;
 						
-						if bmats(x, z, p, b) ~= 1
+						if rating_thresholded(x, z, p) > 0
 							% Then there is a violation
 							
 							violation_mat(x, y, z, p, b) = 1;
@@ -57,10 +80,10 @@ for b = 1 : numel(b_thresholds)
 				end
 			end
 		end
+		
 	end
 	
 end
-
 
 end
 
